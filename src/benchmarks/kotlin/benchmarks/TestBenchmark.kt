42319a57fe41d8ac4181158bcfc1d4c4383dcf52
griffith1deady <71380746+griffith1deady@users.noreply.github.com>
2023-04-28 17:02:25 +0300

    Add benchmark framework

---
+++ src/benchmarks/kotlin/benchmarks/TestBenchmark.kt
@@ -0,0 +1,31 @@
+package benchmarks
+
+import org.openjdk.jmh.annotations.*
+import java.util.concurrent.TimeUnit
+import kotlin.math.cos
+import kotlin.math.sqrt
+
+@State(Scope.Benchmark)
+@Fork(1)
+@Warmup(iterations = 0)
+@Measurement(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
+class TestBenchmark {
+    private var data: TestData = TestData(0.0)
+
+    @Setup
+    fun setUp() {
+        data = TestData(3.0)
+    }
+
+    @Benchmark
+    fun sqrtBenchmark(): Double {
+        return sqrt(data.value)
+    }
+
+    @Benchmark
+    fun cosBenchmark(): Double {
+        return cos(data.value)
+    }
+}
+
+data class TestData(var value: Double)
